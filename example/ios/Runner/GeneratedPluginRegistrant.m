36eb3be171cfaf85b6eb15025a4acb64cea30c6a
瑞宇 <157418979@qq.com>
2022-09-06 18:53:42 +0800

    Initial commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<huawei_analytics/AnalyticsPlugin.h>)
+#import <huawei_analytics/AnalyticsPlugin.h>
+#else
+@import huawei_analytics;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"AnalyticsPlugin"]];
+}
+
+@end
